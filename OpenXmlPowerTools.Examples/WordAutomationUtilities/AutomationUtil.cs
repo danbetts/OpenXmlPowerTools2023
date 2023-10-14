47760842776e5efe45d9188ee03aefc5447d3978
Dan B <daniel.betts@kbr.com>
2023-10-14 17:56:07 +1000

    1. Renamed Project 2. Unloaded AutomationUtil since the project has no access point.

--- OpenXmlPowerTools.Examples/WordAutomationUtilities/AutomationUtil.cs
+++ OpenXmlPowerTools.Examples/WordAutomationUtilities/AutomationUtil.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using Word = Microsoft.Office.Interop.Word;
+using Microsoft.Office.Interop.Word;
 
 namespace OpenXmlPowerTools
 {
@@ -11,24 +9,24 @@ namespace OpenXmlPowerTools
     {
         public static void ProcessFilesUsingWordAutomation(List<string> fileNames)
         {
-            Word.Application app = new Word.Application();
+            Application app = new Application();
             app.Visible = false;
             foreach (string fileName in fileNames)
             {
                 FileInfo fi = new FileInfo(fileName);
                 try
                 {
-                    Word.Document doc = app.Documents.Open(fi.FullName);
+                    Document doc = app.Documents.Open(fi.FullName);
                     doc.Save();
                 }
                 catch (System.Runtime.InteropServices.COMException)
                 {
                     Console.WriteLine("Caught unexpected COM exception.");
-                    ((Microsoft.Office.Interop.Word._Application)app).Quit();
+                    app.Quit();
                     Environment.Exit(0);
                 }
             }
-            ((Microsoft.Office.Interop.Word._Application)app).Quit();
+            app.Quit();
         }
     }
 }
